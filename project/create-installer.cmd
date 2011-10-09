09054f708d81b6b51b54b47d31bf84fac8d14487
Lars Op den Kamp <lars@opdenkamp.eu>
2011-10-09 19:06:37 +0200

    cec: added installer for windows. requires Visual Studio / Visual C++ 2010, NSIS and WinDDK.

---
+++ project/create-installer.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set NSIS="C:\Program Files (x86)\NSIS\makensis.exe"
+set DDK=C:\WinDDK\7600.16385.1
+
+IF "%VS100COMNTOOLS%"=="" (
+  set COMPILER="%ProgramFiles%\Microsoft Visual Studio 10.0\Common7\IDE\VCExpress.exe"
+) ELSE IF EXIST "%VS100COMNTOOLS%\..\IDE\VCExpress.exe" (
+  set COMPILER="%VS100COMNTOOLS%\..\IDE\VCExpress.exe"
+) ELSE IF EXIST "%VS100COMNTOOLS%\..\IDE\devenv.exe" (
+  set COMPILER="%VS100COMNTOOLS%\..\IDE\devenv.exe"
+)
+
+rem Compile libCEC
+echo Cleaning libCEC
+%COMPILER% libcec.sln /clean Debug
+
+echo Compiling libCEC
+%COMPILER% libcec.sln /clean Debug
+%COMPILER% libcec.sln /build Debug
+
+rem Copy driver installer
+echo Copying driver installer
+copy "%DDK%\redist\DIFx\dpinst\MultiLin\amd64\dpinst.exe" ..\dpinst-amd64.exe
+copy "%DDK%\redist\DIFx\dpinst\MultiLin\x86\dpinst.exe" ..\dpinst-x86.exe
+
+rem Run the NSIS installer
+echo Creating the installer
+%NSIS% /V1 /X"SetCompressor /FINAL lzma" "libCEC.nsi"
+
+echo The installer can be found here: libCEC-installer.exe
