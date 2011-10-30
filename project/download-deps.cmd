9511a8b1812eb840030fbbef72f0e9492e9fa688
Lars Op den Kamp <lars.opdenkamp@pulse-eight.com>
2011-10-30 16:12:35 +0100

    win32: fix boost download

--- project/download-deps.cmd
+++ project/download-deps.cmd
@@ -4,9 +4,10 @@ echo Downloading boost
 bin\wget.exe "http://mirrors.xbmc.org/build-deps/win32/boost-1_46_1-xbmc-win32.7z"
 
 echo Extracting boost
-bin\7za.exe "boost-1_46_1-xbmc-win32.7z"
+bin\7za.exe x -y "boost-1_46_1-xbmc-win32.7z"
 
 echo Copying boost
 xcopy boost-1_46_1-xbmc-win32\include\* "..\include\" /E /Q /I /Y
-xcopy boost-1_46_1-xbmc-win32\lib\* "%CUR_PATH%\" /E /Q /I /Y
 
+echo Cleaning up
+del boost-1_46_1-xbmc-win32.7z
