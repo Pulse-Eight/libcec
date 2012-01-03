f26ea36763c4e7cca87a128e87326670699700da
Lars Op den Kamp <lars@opdenkamp.eu>
2012-01-03 15:30:17 +0100

    cec: use CCECCommandHandler::ActivateSource() instead of TransmitActiveSource() in CCECProcessor::SetActiveSource()

--- src/lib/CECProcessor.cpp
+++ src/lib/CECProcessor.cpp
@@ -417,16 +417,7 @@ bool CCECProcessor::SetActiveSource(cec_device_type type /* = CEC_DEVICE_TYPE_RE
 
   m_busDevices[addr]->SetActiveSource();
   if (m_busDevices[addr]->GetPhysicalAddress(false) != 0xFFFF)
-  {
-    bReturn = m_busDevices[addr]->TransmitActiveSource();
-
-    if (bReturn && (m_busDevices[addr]->GetType() == CEC_DEVICE_TYPE_PLAYBACK_DEVICE ||
-        m_busDevices[addr]->GetType() == CEC_DEVICE_TYPE_RECORDING_DEVICE) &&
-        m_busDevices[addr]->GetHandler()->SendDeckStatusUpdateOnActiveSource())
-    {
-      bReturn = ((CCECPlaybackDevice *)m_busDevices[addr])->TransmitDeckStatus(CECDEVICE_TV);
-    }
-  }
+    bReturn = m_busDevices[addr]->GetHandler()->ActivateSource();
 
   return bReturn;
 }
