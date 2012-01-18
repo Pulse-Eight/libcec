3aec622d986b2f3a942f9ba6e4242b624d3650b5
Lars Op den Kamp <lars@opdenkamp.eu>
2012-01-18 11:54:49 +0100

    cec: put the assembly key in AssemblyInfo.cpp

--- src/LibCecSharp/AssemblyInfo.cpp
+++ src/LibCecSharp/AssemblyInfo.cpp
@@ -4,11 +4,6 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-//
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
 [assembly:AssemblyTitleAttribute("LibCecSharp")];
 [assembly:AssemblyDescriptionAttribute("")];
 [assembly:AssemblyConfigurationAttribute("")];
@@ -18,21 +13,9 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
 [assembly:AssemblyVersionAttribute("1.4.0.0")];
 
 [assembly:ComVisible(false)];
-
 [assembly:CLSCompliantAttribute(true)];
-
 [assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
+[assembly:AssemblyKeyFile("LibCecSharp-dev.snk")];
