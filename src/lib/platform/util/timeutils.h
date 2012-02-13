143e675b705eaceb1bb514e63301c02dbcf70600
Lars Op den Kamp <lars@opdenkamp.eu>
2012-02-13 03:02:55 +0100

    platform: added an Init() method in CTimeout, so it can be reset

--- src/lib/platform/util/timeutils.h
+++ src/lib/platform/util/timeutils.h
@@ -107,8 +107,11 @@ namespace PLATFORM
   class CTimeout
   {
   public:
-    CTimeout(uint32_t iTime) :
-      m_iTarget(GetTimeMs() + iTime) {}
+    CTimeout(void) : m_iTarget(0) {}
+    CTimeout(uint32_t iTimeout) { Init(iTimeout); }
+
+    bool IsSet(void) const       { return m_iTarget > 0; }
+    void Init(uint32_t iTimeout) { m_iTarget = GetTimeMs() + iTimeout; }
 
     uint64_t TimeLeft(void) const
     {
