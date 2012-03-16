fe462cdd03ae6f4135af0ae4fc7433113112ead1
Lars Op den Kamp <lars@opdenkamp.eu>
2012-03-16 10:16:47 +0100

    bumped version numbers to 1.5.3 and updated changelogs

--- src/LibCecSharp/AssemblyInfo.cpp
+++ src/LibCecSharp/AssemblyInfo.cpp
@@ -13,7 +13,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.5.2.0")];
+[assembly:AssemblyVersionAttribute("1.5.3.0")];
 
 [assembly:ComVisible(false)];
 [assembly:CLSCompliantAttribute(true)];
