548c81b3c632c90fabc592cfedb3b87fd65f592b
Lars Op den Kamp <lars@opdenkamp.eu>
2012-08-02 19:26:23 +0200

    fixed - deleted a couple of lines too many in 045b2e29de7d187e8586fa3f6b59b9c6470efd31

--- src/lib/implementations/CECCommandHandler.cpp
+++ src/lib/implementations/CECCommandHandler.cpp
@@ -1152,6 +1152,9 @@ bool CCECCommandHandler::ActivateSource(bool bTransmitDelayedCommandsOnly /* = f
     if (bActiveSourceFailed || !bSourceSwitchAllowed)
     {
       LIB_CEC->AddLog(CEC_LOG_DEBUG, "failed to make '%s' the active source. will retry later", m_busDevice->GetLogicalAddressName());
+      CLockObject lock(m_mutex);
+      if (m_iActiveSourcePending == 0)
+        m_iActiveSourcePending = GetTimeMs() + (int64_t)CEC_ACTIVE_SOURCE_SWITCH_RETRY_TIME_MS;
       return false;
     }
     else
