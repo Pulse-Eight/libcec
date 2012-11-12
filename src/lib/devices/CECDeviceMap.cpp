4ba7bfae1e415640550a7515b5b8a910c893a208
Lars Op den Kamp <lars@opdenkamp.eu>
2012-11-13 00:30:09 +0100

    only mark a device as active source from cache if it's powered on

--- src/lib/devices/CECDeviceMap.cpp
+++ src/lib/devices/CECDeviceMap.cpp
@@ -208,6 +208,7 @@ CCECBusDevice *CCECDeviceMap::GetActiveSource(void) const
   for (CECDEVICEMAP::const_iterator it = m_busDevices.begin(); it != m_busDevices.end(); it++)
   {
     if (m_iActiveSource != CEC_INVALID_PHYSICAL_ADDRESS && !it->second->IsActiveSource() &&
+        it->second->GetCurrentPowerStatus() == CEC_POWER_STATUS_ON &&
         m_iActiveSource == it->second->GetCurrentPhysicalAddress())
       it->second->MarkAsActiveSource();
     if (it->second->IsActiveSource())
