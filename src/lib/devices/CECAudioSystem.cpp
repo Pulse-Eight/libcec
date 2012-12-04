298034ea68eded5a4045d563609d64746203c107
Lars Op den Kamp <lars@opdenkamp.eu>
2012-12-04 03:24:42 +0100

    fix initial audiostatus value

--- src/lib/devices/CECAudioSystem.cpp
+++ src/lib/devices/CECAudioSystem.cpp
@@ -47,7 +47,7 @@ using namespace PLATFORM;
 CCECAudioSystem::CCECAudioSystem(CCECProcessor *processor, cec_logical_address address, uint16_t iPhysicalAddress /* = CEC_INVALID_PHYSICAL_ADDRESS */) :
     CCECBusDevice(processor, address, iPhysicalAddress),
     m_systemAudioStatus(CEC_SYSTEM_AUDIO_STATUS_ON),
-    m_audioStatus(CEC_AUDIO_MUTE_STATUS_MASK)
+    m_audioStatus(CEC_AUDIO_VOLUME_STATUS_UNKNOWN)
 {
   m_type = CEC_DEVICE_TYPE_AUDIO_SYSTEM;
 }
