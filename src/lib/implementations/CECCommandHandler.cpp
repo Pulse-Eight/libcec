14004a262acba6d284ce6489ef18468bdc1a6e5c
Lars Op den Kamp <lars@opdenkamp.eu>
2013-12-12 02:48:04 +0100

    transmit our physical address when the TV sends it's physical address

--- src/lib/implementations/CECCommandHandler.cpp
+++ src/lib/implementations/CECCommandHandler.cpp
@@ -455,6 +455,13 @@ int CCECCommandHandler::HandleReportPhysicalAddress(const cec_command &command)
   {
     uint16_t iNewAddress = ((uint16_t)command.parameters[0] << 8) | ((uint16_t)command.parameters[1]);
     SetPhysicalAddress(command.initiator, iNewAddress);
+
+    if (command.initiator == CECDEVICE_TV)
+    {
+      CCECBusDevice* primary = m_processor->GetPrimaryDevice();
+      if (primary)
+        primary->TransmitPhysicalAddress(false);
+    }
     return COMMAND_HANDLED;
   }
   return CEC_ABORT_REASON_INVALID_OPERAND;
