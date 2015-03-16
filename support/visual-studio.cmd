072a4eb16cd975bbb9b9ff037deab9bda3a606a4
Lars Op den Kamp <lars@opdenkamp.eu>
2015-03-16 22:07:02 +0100

    generate debug mode visual studio solutions

--- support/visual-studio.cmd
+++ support/visual-studio.cmd
@@ -7,14 +7,16 @@ SET BASEDIR=%CD%\..
 SET BUILDBASEDIR=%BASEDIR%\cmake-build
 
 rem generate visual studio project files
-call build-platform.cmd amd64 Release
-call generate-cmake.cmd amd64 vs "%BASEDIR%" "%BUILDBASEDIR%\build64" Release
+call build-platform.cmd amd64 Debug
+call generate-cmake.cmd amd64 vs "%BASEDIR%" "%BUILDBASEDIR%\build64" Debug
 
-call build-platform.cmd x86 Release
-call generate-cmake.cmd x86 vs "%BASEDIR%" "%BUILDBASEDIR%\build" Release
+call build-platform.cmd x86 Debug
+call generate-cmake.cmd x86 vs "%BASEDIR%" "%BUILDBASEDIR%\build" Debug
 
 rem cls
 echo Visual Studio solutions can be found in:
 echo 32 bits: %BASEDIR%\cmake-build\build\libcec.sln
 echo 64 bits: %BASEDIR%\cmake-build\build64\libcec.sln
+echo.
+echo These projects only compile in debug mode
 pause
