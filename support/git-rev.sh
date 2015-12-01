3de451fd4ee7fda95bd3e482a41cf6add4e0b2a6
Lars Op den Kamp <lars@opdenkamp.eu>
2015-12-01 21:28:41 +0100

    added: detect dirty trees

--- support/git-rev.sh
+++ support/git-rev.sh
@@ -5,8 +5,13 @@ if git rev-parse --git-dir > /dev/null 2>&1; then
   last_tag=`git describe --tags --abbrev=0`
   last_hash=`git --no-pager log --abbrev=7 -n 1 --pretty=format:"%h"`
   commits_since_tag=`git log ${last_tag}..HEAD --oneline | wc -l`
+  git_dirty=`git diff HEAD | wc -l`
   if [ $commits_since_tag -gt 0 ]; then
-    echo "${last_tag}+${commits_since_tag}-${last_hash}"
+    dirty=""
+    if [ $git_dirty -gt 0 ]; then
+      dirty="~dirty"
+    fi
+    echo "${last_tag}+${commits_since_tag}-${last_hash}${dirty}"
   else
     echo $last_tag
   fi
