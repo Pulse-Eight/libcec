6cf77a079ade4030e72f0a666daa679310d7c941
Lars Op den Kamp <lars@opdenkamp.eu>
2015-12-01 11:41:11 +0100

    fixed/changed: git-rev.sh script

--- support/git-rev.sh
+++ support/git-rev.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 ## cmake doesn't read the variable when it doesn't end with a newline, and I haven't figured out how to have it add a newline directly...
-if [ -d .git ]; then
-  echo "`git --no-pager log --abbrev=7 -n 1 --pretty=format:"%h"`"
+if git rev-parse --git-dir > /dev/null 2>&1; then
+  last_tag=`git describe --tags --abbrev=0`
+  last_hash=`git --no-pager log --abbrev=7 -n 1 --pretty=format:"%h"`
+  commits_since_tag=`git log ${last_tag}..HEAD --oneline | wc -l`
+  if [ $commits_since_tag -gt 0 ]; then
+    echo "${last_tag}+${commits_since_tag}-${last_hash}"
+  else
+    echo $last_tag
+  fi
 else
   echo "<unknown>"
 fi
