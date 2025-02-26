87101d2f303358d4bb0f5e817f51537c79a2ad2a
Lars Op den Kamp <lars@opdenkamp.eu>
2025-02-26 10:28:29 +0100

    fixed: don't complain about unused parameters in swig generated code

--- src/libcec/cmake/CheckPlatformSupport.cmake
+++ src/libcec/cmake/CheckPlatformSupport.cmake
@@ -326,6 +326,7 @@ else()
     set_property(SOURCE libcec.i PROPERTY SWIG_MODULE_NAME cec)
     SWIG_ADD_LIBRARY(pycec LANGUAGE python TYPE MODULE SOURCES libcec.i)
     SWIG_LINK_LIBRARIES(${SWIG_MODULE_pycec_REAL_NAME} cec ${PYTHON_LIBRARIES})
+    target_compile_options(pycec PUBLIC "-Wno-unused-parameter")
 
     if(WIN32)
       install(TARGETS     ${SWIG_MODULE_pycec_REAL_NAME}
