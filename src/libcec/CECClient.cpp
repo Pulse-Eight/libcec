Pulse-Eight/libcec

commit 05fc6d01e6e3f01164f3e844f88ece4bf19e16c9

fixed: don't call the config updated callback if nothing has changed. auto set wake devices or activate source so these values match what happens on the tv

src/libcec/CECClient.cpp
@@ -214,12 +214,7 @@ bool CCECClient::SetHDMIPort(const cec_logical_address iBaseDevice, const uint8_
     }
   }
 
-  // and set the address
-  SetDevicePhysicalAddress(iPhysicalAddress);
-
-  QueueConfigurationChanged(m_configuration);
-
-  return bReturn;
+  return SetPhysicalAddress(iPhysicalAddress);
 }
 
 void CCECClient::ResetPhysicalAddress(void)
@@ -274,6 +269,11 @@ bool CCECClient::SetPhysicalAddress(const libcec_configuration &configuration)
 
 bool CCECClient::SetPhysicalAddress(const uint16_t iPhysicalAddress)
 {
+  if (m_configuration.iPhysicalAddress == iPhysicalAddress)
+  {
+    return true;
+  }
+
   // update the configuration
   {
     CLockObject lock(m_mutex);
@@ -882,6 +882,7 @@ bool CCECClient::GetCurrentConfiguration(libcec_configuration &configuration)
 
 bool CCECClient::SetConfiguration(const libcec_configuration &configuration)
 {
+  bool notifyUpper = false;
   libcec_configuration defaultSettings;
   bool bIsRunning(m_processor && m_processor->CECInitialised());
   CCECBusDevice *primary = bIsRunning ? GetPrimaryDevice() : NULL;
@@ -901,9 +902,12 @@ bool CCECClient::SetConfiguration(const libcec_configuration &configuration)
   // update the TV vendor override
   SetTVVendorOverride((cec_vendor_id)configuration.tvVendor);
 
-  // just copy these
   {
     CLockObject lock(m_mutex);
+    bool activeSourceChanged = (configuration.bActivateSource != m_configuration.bActivateSource);
+    bool wakeTvChanged = (configuration.wakeDevices.IsSet(CECDEVICE_TV) != m_configuration.wakeDevices.IsSet(CECDEVICE_TV));
+
+    // just copy these
     m_configuration.bActivateSource            = configuration.bActivateSource;
     m_configuration.bGetSettingsFromROM        = configuration.bGetSettingsFromROM;
     m_configuration.wakeDevices                = configuration.wakeDevices;
@@ -923,6 +927,27 @@ bool CCECClient::SetConfiguration(const libcec_configuration &configuration)
     if ((configuration.bAutoPowerOn == 0) || (configuration.bAutoPowerOn == 1))
       m_configuration.bAutoPowerOn             = configuration.bAutoPowerOn;
 #endif
+
+    if (activeSourceChanged)
+    {
+      // activate source will wake up the TV too
+      if ((m_configuration.bActivateSource == 1) && !m_configuration.wakeDevices.IsSet(CECDEVICE_TV))
+      {
+        m_configuration.wakeDevices.Set(CECDEVICE_TV);
+        LIB_CEC->AddLog(CEC_LOG_DEBUG, "enable tv wake up");
+        notifyUpper = true;
+      }
+    }
+    if (wakeTvChanged)
+    {
+      // disabling waking up the TV disables activate source too
+      if (!m_configuration.wakeDevices.IsSet(CECDEVICE_TV) && (m_configuration.bActivateSource != 0))
+      {
+        m_configuration.bActivateSource = 0;
+        LIB_CEC->AddLog(CEC_LOG_DEBUG, "disable active source");
+        notifyUpper = true;
+      }
+    }
   }
 
   bool bNeedReinit(false);
@@ -970,7 +995,11 @@ bool CCECClient::SetConfiguration(const libcec_configuration &configuration)
     primary->ActivateSource();
   }
 
-  LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s: double tap timeout = %ums, repeat rate = %ums, release delay = %ums", __FUNCTION__, DoubleTapTimeoutMS(), m_configuration.iButtonRepeatRateMs, m_configuration.iButtonReleaseDelayMs);
+  if (notifyUpper)
+  {
+    QueueConfigurationChanged(m_configuration);
+  }
+
   return true;
 }
 
@@ -1262,6 +1291,10 @@ void CCECClient::SetTVVendorOverride(const cec_vendor_id id)
 {
   {
     CLockObject lock(m_mutex);
+    if (m_configuration.tvVendor == id)
+    {
+      return;
+    }
     m_configuration.tvVendor = id;
   }
 
@@ -1287,11 +1320,14 @@ void CCECClient::SetOSDName(const std::string &strDeviceName)
 {
   {
     CLockObject lock(m_mutex);
-    if (!strncmp(m_configuration.strDeviceName, strDeviceName.c_str(), LIBCEC_OSD_NAME_SIZE)) return;
-    snprintf(m_configuration.strDeviceName, LIBCEC_OSD_NAME_SIZE, "%s", strDeviceName.c_str());
+    char buf[LIBCEC_OSD_NAME_SIZE + 1] = { 0 };
+    strncpy(buf, strDeviceName.c_str(), LIBCEC_OSD_NAME_SIZE);
+    if (!strncmp(m_configuration.strDeviceName, buf, LIBCEC_OSD_NAME_SIZE))
+      return;
+    strncpy(m_configuration.strDeviceName, buf, LIBCEC_OSD_NAME_SIZE);
+    LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s - using OSD name '%s'", __FUNCTION__, buf);
   }
 
-  LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s - using OSD name '%s'", __FUNCTION__, strDeviceName.c_str());
 
   CCECBusDevice *primary = GetPrimaryDevice();
   if (primary && primary->GetCurrentOSDName() != strDeviceName.c_str())
@@ -1348,10 +1384,16 @@ bool CCECClient::AutodetectPhysicalAddress(void)
 
 void CCECClient::SetClientVersion(uint32_t version)
 {
-  LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s - using client version '%s'", __FUNCTION__, CCECTypeUtils::VersionToString(version).c_str());
-
-  CLockObject lock(m_mutex);
-  m_configuration.clientVersion = (uint32_t)version;
+  bool changed;
+  {
+    CLockObject lock(m_mutex);
+    changed = (m_configuration.clientVersion != version);
+    m_configuration.clientVersion = version;
+  }
+  if (changed)
+  {
+    LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s - using client version '%s'", __FUNCTION__, CCECTypeUtils::VersionToString(version).c_str());
+  }
 }
 
 uint32_t CCECClient::GetClientVersion(void)
@@ -1371,10 +1413,12 @@ bool CCECClient::SetDeviceTypes(const cec_device_type_list &deviceTypes)
     m_configuration.deviceTypes = deviceTypes;
   }
 
-  SaveConfiguration(m_configuration);
-
+  
   if (bNeedReinit)
+  {
     LIB_CEC->AddLog(CEC_LOG_DEBUG, "%s - using primary device type '%s'", __FUNCTION__, ToString(deviceTypes[0]));
+    SaveConfiguration(m_configuration);
+  }
 
   return bNeedReinit;
 }

commit a39e23098c06169af1a19c421d4dc7f8ff9cd0ce

fix physical address auto detection Caused by https://github.com/Pulse-Eight/libcec/commit/05fc6d01e6e3f01164f3e844f88ece4bf19e16c9

src/libcec/CECClient.cpp
@@ -214,7 +214,12 @@ bool CCECClient::SetHDMIPort(const cec_logical_address iBaseDevice, const uint8_
     }
   }
 
-  return SetPhysicalAddress(iPhysicalAddress);
+  // and set the address
+  SetDevicePhysicalAddress(iPhysicalAddress);
+
+  QueueConfigurationChanged(m_configuration);
+
+  return bReturn;
 }
 
 void CCECClient::ResetPhysicalAddress(void)
@@ -269,11 +274,6 @@ bool CCECClient::SetPhysicalAddress(const libcec_configuration &configuration)
 
 bool CCECClient::SetPhysicalAddress(const uint16_t iPhysicalAddress)
 {
-  if (m_configuration.iPhysicalAddress == iPhysicalAddress)
-  {
-    return true;
-  }
-
   // update the configuration
   {
     CLockObject lock(m_mutex);