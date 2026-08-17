d7db0503f4c105cefcecd5cd82f97e23e0f06db1
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2026-08-17 19:18:12 +0100

    use recent MIT-licensed adl_*.h
    The headers
    
     src/libcec/platform/adl/adl_defines.h
     src/libcec/platform/adl/adl_sdk.h
     src/libcec/platform/adl/adl_structures.h
    
    had a weird license that did not grant permission to use, copy, modify
    or redistribute. AMD has republished more recent versions of these
    headers under the MIT licence as the GPUOpen display-library SDK.
    These newer versions are taken from
    
     https://github.com/GPUOpen-LibrariesAndSDKs/display-library/tree/master/include
    
    Only one tiny adjustment is needed. ADL's headers use the memory
    allocation callback __stdcall unconditionally. It used to be defined
    away for non-Windows inside adl_sdk.h itself; the ADL SDK 18 headers
    no longer do that, so here it is done in adl-edid.h to make updating
    the above headers easier.

--- src/libcec/platform/adl/adl-edid.h
+++ src/libcec/platform/adl/adl-edid.h
@@ -38,6 +38,13 @@
 #include "platform/util/edid.h"
 
 #if !defined(__WINDOWS__)
+// ADL's headers spell the memory allocation callback __stdcall unconditionally.
+// It used to be defined away for non-Windows inside adl_sdk.h itself; the ADL
+// SDK 18 headers no longer do that, so do it here and leave them pristine.
+#if !defined(__stdcall)
+#define __stdcall
+#endif
+
 #include "adl_sdk.h"
 #include <dlfcn.h>
 #include <stdlib.h>	
